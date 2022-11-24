7c81b021d5ae6d0a6865d19464b1f3352cf9dce9
ranebhushan <bhushanrane13@gmail.com>
2022-11-23 19:05:09 -0500

    Updated job file for cluster usage

--- run.sh
+++ run.sh
@@ -2,15 +2,27 @@
 
 #SBATCH -N 1
 #SBATCH -n 4
-#SBATCH --mem 64G
+#SBATCH --mem=64G
 #SBATCH --gres=gpu:1
 #SBATCH -C A100
-#SBATCH -p short
-#SBATCH -t 20:00:00
-#SBATCH --output=slurm_outputs/rljob_%j.out
-#SBATCH --error=slurm_outputs/rljob_error_%j.out
+#SBATCH -p long
+#SBATCH -t 167:59:59
+#SBATCH -o slurm_outputs/rljob_%j.out
+#SBATCH --error slurm_outputs/rljob_%j.err
 #SBATCH -J rl_project
+#SBATCH --mail-user=barane@wpi.edu
+#SBATCH --mail-type=ALL
 
 echo "RL Job running on $(hostname)"
+
+echo "Loading Python Virtual Environment"
+
+source ~/cs525_project/rl_project/bin/activate
+
+module load python/3.9.12/uabo2y2
+module load cuda11.7/toolkit/11.7.1
+module load cudnn8.5-cuda11.7/8.5.0.96
+
+echo "Running Python Code"
 
 python3 src/main.py --train_config_path=configs/DQN.yaml --env_config_path=configs/highway-env_config.json

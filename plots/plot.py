eff1be190503b7cf7fa92d8a19d0d27a7706e4de
Denny Boby <dennyboby123@gmail.com>
2022-12-06 02:55:19 -0500

    updated plot script

--- plots/plot.py
+++ plots/plot.py
@@ -2,13 +2,14 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import deque
 import sys, os
 sys.path.insert(1, os.getcwd() + '/logs')
 
 
 # File format: [Date Time,Episode,Reward,Epsilon,Loss,Max. Reward,Mean Reward]
 
-file_name = 'DDDQN-2022-12-03--18-44-20.csv'
+file_name = 'DQN-2022-12-04--03-30-14.csv'
 
 def read_csv(filename):
     logs_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/logs/'
@@ -17,30 +18,40 @@ def read_csv(filename):
 
 def plot_loss(data):
     plt.plot(data['Episode'].to_numpy(), data['Loss'].to_numpy())
-    plt.title("Episode v/s Loss")
-    plt.xlabel("Episode")
+    plt.title("Loss vs. No.of Episodes")
+    plt.xlabel("No.of Episodes")
     plt.ylabel("Loss")
+    plt.savefig('DQN_loss.png',dpi=300,bbox_inches='tight')
     plt.show()
 
 def plot_mean_reward(data):
+    # scores = deque(maxlen=100)
+    # mean_reward = np.array([])
+    # for reward in data['Reward'].to_numpy():
+    #     scores.append(reward)
+    #     mean_reward = np.append(mean_reward, np.mean(scores))
+    # plt.plot(data['Episode'].to_numpy(), mean_reward) 
     plt.plot(data['Episode'].to_numpy(), data['Mean Reward'].to_numpy()) 
-    plt.title("Episode v/s Mean Reward")
-    plt.xlabel("Episode")
+    plt.title("Mean Reward vs. No.of Episodes")
+    plt.xlabel("No.of Episodes")
     plt.ylabel("Mean Reward")
+    plt.savefig('DQN_mean_reward.png',dpi=300,bbox_inches='tight')
     plt.show() 
 
 def plot_reward(data):
     plt.plot(data['Episode'].to_numpy(), data['Reward'].to_numpy()) 
-    plt.title("Episode v/s Reward")
-    plt.xlabel("Episode")
-    plt.ylabel("Reward")    
+    plt.title("Reward vs. No.of Episodes")
+    plt.xlabel("No.of Episodes")
+    plt.ylabel("Reward")   
+    plt.savefig('DQN_reward.png',dpi=300,bbox_inches='tight') 
     plt.show() 
 
 def plot_epsilon(data):
     plt.plot(data['Episode'].to_numpy(), data['Epsilon'].to_numpy()) 
-    plt.title("Episode v/s Epsilon")
-    plt.xlabel("Episode")
-    plt.ylabel("Epsilon")    
+    plt.title("Epsilon vs. No.of Episodes")
+    plt.xlabel("No.of Episodes")
+    plt.ylabel("Epsilon")  
+    plt.savefig('DQN_epsilon.png',dpi=300,bbox_inches='tight')  
     plt.show() 
 
 def main():
@@ -52,12 +63,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# y = np.array([])
-
-
-
-# x = np.arange(start=30, stop=30*(y.shape[0]+1), step=30)
-
-# plt.plot(x, y)
-# plt.show()
